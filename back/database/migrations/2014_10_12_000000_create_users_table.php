4257d00078f9e6e4d2db40f7b0e2b796c5d2db3a
Manchabeti <masuguerre@gmail.com>
2023-12-07 19:48:24 +0100

    back

---
+++ back/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->string('UserName')->primary();
+            $table->decimal('Dinero', 20, 0)->nullable();
+            $table->tinyInteger('EsAdmin')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
